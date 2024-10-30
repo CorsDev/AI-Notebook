d6fad06f5806591dcf5fbd079798387655f046a5
Giuseppe Corsino <giuseppecorsino@Giuseppes-MacBook-Pro.local>
2024-10-30 01:02:38 +0100

    registerCommand cleanup

--- src/extension.ts
+++ src/extension.ts
@@ -3,28 +3,14 @@
 import * as vscode from 'vscode';
 import { AINotebookSerializer } from './notebook/serializer';
 import { AINotebookController } from './notebook/controller';
-import { showModelSelector } from './utils/selectModel';
+import { selectModel, showModelSelector } from './utils/selectModel';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	const serializer = vscode.workspace.registerNotebookSerializer("ainotebook", new AINotebookSerializer());
 	const aiNotebookController = new AINotebookController();
-	vscode.commands.registerCommand("ainotebook.selectmodel", async () => {
-		if(!vscode.window.activeNotebookEditor){
-			vscode.window.showErrorMessage("No AI Notebook active");
-			return;
-		}
-
-		const models = await vscode.lm.selectChatModels();
-
-		if(!models.length) {
-			vscode.window.showInformationMessage("No models available");
-			return;
-		}
-
-		showModelSelector(models, vscode.window.activeNotebookEditor?.notebook);
-	});
+	vscode.commands.registerCommand("ainotebook.selectmodel", selectModel);
 
 	context.subscriptions.push(serializer);
 	context.subscriptions.push(aiNotebookController.controller);
